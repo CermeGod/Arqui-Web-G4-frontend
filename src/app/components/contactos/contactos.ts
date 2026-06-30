812114d181b125d539d26a6dd4b7081a63008a51
AaronSalvador <aaronlaura128@gmail.com>
2026-06-30 10:04:39 -0500

    avance seguridad

--- src/app/components/contactos/contactos.ts
+++ src/app/components/contactos/contactos.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { Menu } from '../menu/menu';
+import { ContactosListar } from './contactos-listar/contactos-listar';
 
 @Component({
   selector: 'app-contactos',
-  imports: [],
+  imports: [RouterOutlet, Menu, ContactosListar],
   templateUrl: './contactos.html',
   styleUrl: './contactos.css',
 })
 export class Contactos {
-
+  constructor(public route: ActivatedRoute) {}
 }
